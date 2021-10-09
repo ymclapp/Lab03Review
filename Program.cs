cc43210cf5ef08dd647ed507005f58889ad8af1e
ymclapp <ymclapp@yahoo.com>
2021-10-09 16:55:20 -0500

    setup base files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab03Review
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
